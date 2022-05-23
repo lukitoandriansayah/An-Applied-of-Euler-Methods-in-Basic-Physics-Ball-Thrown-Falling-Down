f297f0c46ae767ce1d9cac3e94d5588434195649
Lukito Andriansyah <104261463+lukitoandriansayah@users.noreply.github.com>
2022-05-23 12:58:43 +0700

    Update and rename Data_1_a_1.py to 5-Membuat-DataFrame-Kasus-1-(Data-Utama).py

--- 5-Membuat-DataFrame-Kasus-1-(Data-Utama).py
+++ 5-Membuat-DataFrame-Kasus-1-(Data-Utama).py
@@ -1,11 +1,17 @@
+"""
+PERHATIAN!!!
+Silakan simpan file ini dengan nama Data_1_a_1 dalam format .py
+untuk memudahkan.
+"""
+
+
 import pandas as pd
 import numpy as np
 
 """Known Variables in SI units for Case 1"""
 v_0 = 35 * (1000/3600)
 v_term = 150 * (1000/3600)
-t_total = float(10)                         #t_total di sini adalah waktu yang diketahui dalam data
-                                            #bukan waktu secara keseluruhan
+t_total = float(10)                         #t_total di sini adalah waktu yang diketahui dalam data bukan waktu secara keseluruhan
 t_df = t_total + 0.01                       #Penggunaan 0.01 optional
 """
 Using Methode Euler
@@ -13,10 +19,9 @@ Using Methode Euler
 t_0 = 0
 x_0 = 0
 a_0 = float(9.81)                       #variabel a_0 disini merupakan nilai percepatan gravitasi bumi
-
-n = 5000                                #Pemilihan nilai n optional, karena ini akan digunakan sebagai
-                                        #acuan banyaknya jumlah baris datayang ingin dibuat dalam dataframe
+n = 5000                                #Pemilihan nilai n optional, karena ini akan digunakan sebagai acuan banyaknya jumlah baris data yang ingin dibuat dalam dataframe
 dt = 6 * t_df / n                       #Penggunaan angka 6 adalah optional, semakin besar semaik baik
+
 #Step1: Membuat kerangka data frame
 data =[t_0, x_0, v_0]
 data.append(a_0*(1-(data[2]**2/v_term**2)))
@@ -26,6 +31,7 @@ while n not in rows:
 index = np.array(rows)
 df = pd.DataFrame(columns=['t','x','v','a'], index= index)
 df.loc[0] = data
+
 #Step2: Melengkapi Kerangka Dataframe
 i = 1
 while np.isnan(df['t'].loc[i]).sum() > 0 and i < n:
@@ -37,13 +43,11 @@ while np.isnan(df['t'].loc[i]).sum() > 0 and i < n:
 df_euler_methods = df.dropna(axis=0)
 df_euler_methods.to_csv('Data Frame Nomor 1_a_1.csv', index=False, encoding='utf-8', quoting=1)
 
-# import the module
+#Step3: Mengimport DataFrame ke dalam DataBase MySQL
 from sqlalchemy import create_engine
 import pymysql
 import mysql
-# create sqlalchemy engine
 engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
                       .format(user="***", pw="***",
                       db="Euler_Methods"))  # Ubah *** sesuai karakter MySQL masing-masing
-# Insert whole DataFrame into MySQL
 df_euler_methods.to_sql('Euler_Method_No_1_a_1', con = engine, if_exists = 'append', chunksize = 1000,index=False)
