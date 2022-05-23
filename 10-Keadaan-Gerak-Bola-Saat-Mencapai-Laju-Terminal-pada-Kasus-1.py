a4bd94f9d111e8252f0d59cae33969d8111dc73f
Lukito Andriansyah <104261463+lukitoandriansayah@users.noreply.github.com>
2022-05-23 13:28:32 +0700

    Update and rename Pertanyaan_1_a_1.py to 10-Keadaan-Gerak-Bola-Saat-Mencapai-Laju-Terminal-pada-Kasus-1.py

--- 10-Keadaan-Gerak-Bola-Saat-Mencapai-Laju-Terminal-pada-Kasus-1.py
+++ 10-Keadaan-Gerak-Bola-Saat-Mencapai-Laju-Terminal-pada-Kasus-1.py
@@ -1,10 +1,12 @@
 """
 Keadaan gerak setelah mencapai laju terminal?
+
+Keadaan gerak di sini mencaku waktu, posisi, laju, dan percepatan.
 """
 import mysql.connector as msql
 # Execute query
-conn = msql.connect(host='localhost', user='root',
-                         password='V-k21122015', db="Euler_Methods")
+conn = msql.connect(host='***', user='***',
+                         password='***', db="Euler_Methods")  #Ubah *** sesuai karakter MySQL masing-masing
 cursor = conn.cursor()
 coord_v_term = "SELECT t, x, v, a " \
                "FROM Euler_Method_No_1_a_1 " \
